0ad98ec7e9e6b32b8f7d2bce64d33c1abc48f77e
Rob <robertorodesluchorodev@gmail.com>
2025-04-24 12:20:38 +0200

    api is working

--- HolidaysApp-Backend/application/src/main/java/com/rob/domain/models/services/impl/OrganizationServiceImpl.java
+++ HolidaysApp-Backend/application/src/main/java/com/rob/domain/models/services/impl/OrganizationServiceImpl.java
@@ -1,34 +1,64 @@
 package com.rob.domain.models.services.impl;
 
 import com.rob.domain.models.entities.Organization;
+import com.rob.domain.models.repository.OrganizationRepository;
 import com.rob.domain.models.services.OrganizationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
+@Service
 public class OrganizationServiceImpl implements OrganizationService {
+
+    @Autowired
+    private OrganizationRepository organizationRepository;
+
+    @Override
+    public Optional<List<Organization>> getAllOrganizations() {
+        return Optional.of(organizationRepository.findAll());
+    }
+
     @Override
     public Optional<Organization> saveOrganization(Organization organization) throws Exception {
-        Optional<Organization> existingOrganization = findOrganizationById(organization.getId());
+        Optional<Organization> existingOrganization = findOrganizationById(organization.getOrgId());
+        if(existingOrganization.isEmpty()) {
+            organizationRepository.save(organization);
+            return Optional.of(organization);
+        }
+        return Optional.empty();
     }
 
     @Override
     public Optional<Organization> findOrganizationById(Long id) {
-        return Optional.empty();
+        Optional<Organization> existingOrganization = findOrganizationById(id);
+        return existingOrganization;
     }
 
     @Override
     public Optional<List<Organization>> findByName(String name) {
-        return Optional.empty();
+        List<Organization> organizations = organizationRepository.findByName(name);
+        return Optional.of(organizations);
     }
 
     @Override
     public Optional<Organization> updateOrganization(Organization organization) throws Exception {
+        Optional<Organization> existingOrganization = findOrganizationById(organization.getOrgId());
+        if(existingOrganization.isPresent()) {
+            organizationRepository.save(organization);
+            return Optional.of(organization);
+        }
         return Optional.empty();
     }
 
     @Override
     public Optional<Organization> deleteOrganizationById(Long id) {
+        Optional<Organization> existingOrganization = findOrganizationById(id);
+        if(existingOrganization.isPresent()) {
+            organizationRepository.deleteById(id);
+            return existingOrganization;
+        }
         return Optional.empty();
     }
 }
