4e93ebaa9e8067144268fccaf27e659f4ef8cb41
Rob <robertorodesluchorodev@gmail.com>
2025-04-30 16:39:18 +0200

    added module dependecy of boot in postgres-repository's module

---
+++ HolidaysApp-Backend/driven/postgres-repository/src/main/java/com/rob/main/driven/repositories/models/Organization.java
@@ -0,0 +1,24 @@
+package com.rob.main.driven.repositories.models;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "organizations")
+public class Organization {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "org_id", nullable = false)
+    private Integer id;
+
+    @NotNull
+    @Column(name = "name", nullable = false, length = Integer.MAX_VALUE)
+    private String name;
+
+}
