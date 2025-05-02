a65109fc5a5605e80b2d9ebb469e04fcfb19a0e3
Rob <robertorodesluchorodev@gmail.com>
2025-05-02 08:47:25 +0200

    open api documentation done

--- HolidaysApp-Backend/driving/api-rest/src/main/java/com/rob/driving/controllers/adapters/OrganizationControllerAdapter.java
+++ HolidaysApp-Backend/driving/api-rest/src/main/java/com/rob/driving/controllers/adapters/OrganizationControllerAdapter.java
@@ -2,14 +2,18 @@ package com.rob.driving.controllers.adapters;
 
 import com.rob.application.ports.driving.OrganizationServicePort;
 import com.rob.domain.models.Organization;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.annotation.PostConstruct;
 import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import io.swagger.v3.oas.annotations.tags.Tag;
 
 import java.util.List;
 
@@ -27,6 +31,12 @@ public class OrganizationControllerAdapter {
         System.out.println("Controller loaded!");
     }
 
+    @Tag(name = "get", description = "Get all organizations of Organizations APIs")
+    @Operation(operationId = "getAllOrganizations",summary = "Get all organizations", description = "Get all organizations of Organizations APIs")
+    @ApiResponses(value = {
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "200", description = "OK"),
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "500", description = "Internal Server Error")
+    })
     @GetMapping
     public List<Organization> getAllOrganizations() {
         return organizationServicePort.getAllOrganizations();
