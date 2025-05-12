eccccec1cce11c16a068263b7f2d8e5480a26c14
Rob <robertorodesluchorodev@gmail.com>
2025-05-12 13:26:47 +0200

    bacckend with open api

--- HolidaysApp-Backend/driving/api-rest/src/main/java/com/rob/driving/controllers/adapters/OrganizationControllerAdapter.java
+++ HolidaysApp-Backend/driving/api-rest/src/main/java/com/rob/driving/controllers/adapters/OrganizationControllerAdapter.java
@@ -8,20 +8,25 @@ import jakarta.annotation.PostConstruct;
 import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
-import org.springframework.stereotype.Service;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import io.swagger.v3.oas.annotations.tags.Tag;
 
+import java.util.ArrayList;
 import java.util.List;
+
+import com.rob.driving.api.OrganizationsApi;
+import com.rob.driving.dtos.OrganizationDTO;
+
+
 
 @RestController
 @AllArgsConstructor
 @RequiredArgsConstructor
 @RequestMapping("/api-rest/organizations")
-public class OrganizationControllerAdapter {
+public class OrganizationControllerAdapter implements OrganizationsApi {
 
     @Autowired
     OrganizationServicePort organizationServicePort;
@@ -35,11 +40,23 @@ public class OrganizationControllerAdapter {
     @Operation(operationId = "getAllOrganizations",summary = "Get all organizations", description = "Get all organizations of Organizations APIs")
     @ApiResponses(value = {
             @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "200", description = "OK"),
-            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "500", description = "Internal Server Error")
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "400", description = "Wrong Request – invalid parameters"),
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "401", description = "Not authorized – invalid or expired token"),
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "403", description = "Denied – no permission to access the resource"),
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "404", description = "Organization not found"),
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "500", description = "Internal Server Error"),
     })
     @GetMapping
-    public List<Organization> getAllOrganizations() {
-        return organizationServicePort.getAllOrganizations();
+    public ResponseEntity<List<OrganizationDTO>> getAllOrganizations() {
+        List<OrganizationDTO> organizationsDtos = new ArrayList<>();
+        List<Organization> organizations = organizationServicePort.getAllOrganizations();
+        for (Organization organization : organizations) {
+            OrganizationDTO organizationDTO = new OrganizationDTO();
+            organizationDTO.setId(organization.getId());
+            organizationDTO.setName(organization.getName());
+            organizationsDtos.add(organizationDTO);
+        }
+        return ResponseEntity.ok(organizationsDtos);
         // try catch con el error que lo coga de los use case
         // ej: error 404 y un mensaje
     }
