98560c5215e1e440b151ab2867a423246e681c20
Rob <robertorodesluchorodev@gmail.com>
2025-05-12 13:02:53 +0200

    sum

--- HolidaysApp-Frontend/src/app/components/auth/field-config.model.ts
+++
@@ -1,6 +0,0 @@
-export interface FieldConfig {
-    key: string;
-    label: string;
-    type: 'text' | 'email' | 'password';
-    modes: ('login' | 'register')[];
-}
--- HolidaysApp-Frontend/src/app/models/Holiday.ts
+++
@@ -1,8 +0,0 @@
-export default interface Holiday {
-    holiday_id?: number;         // opcional para nuevas inserciones
-    user_id: number;
-    holiday_start_date: Date;
-    holiday_end_date: Date;
-    review_date?: Date;
-    review_comment?: string;
-}
