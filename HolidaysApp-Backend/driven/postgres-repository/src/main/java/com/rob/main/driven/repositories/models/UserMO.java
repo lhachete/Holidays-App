adbfa6f158818f4ce5fd4e1cc73e239d4bbba1bf
lhachete <mhmonllor@hotmail.com>
2025-05-16 08:50:11 +0200

    Funciona registro desde postman

--- HolidaysApp-Backend/driven/postgres-repository/src/main/java/com/rob/main/driven/repositories/models/UserMO.java
+++ HolidaysApp-Backend/driven/postgres-repository/src/main/java/com/rob/main/driven/repositories/models/UserMO.java
@@ -14,8 +14,15 @@ import org.hibernate.annotations.ColumnDefault;
 @Table(name = "users")
 public class UserMO {
     @Id
-    //@GeneratedValue(strategy = GenerationType.IDENTITY)
-    @ColumnDefault("nextval('users_user_id_seq')")
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "users_user_id_seq"
+    )
+    @SequenceGenerator(
+            name = "users_user_id_seq",
+            sequenceName = "users_user_id_seq",
+            allocationSize = 1
+    )
     @Column(name = "user_id", nullable = false)
     private Integer id;
 
